86ab5d809ba71a5db99ce2bd4ce01c2e3aa17978
YuriyMikh <pamukkale1717@gmail.com>
2023-06-27 00:34:05 +0300

    fix handleDelete method in App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -44,9 +44,9 @@ export class App extends Component {
   };
 
   handleDelete = id => {
-    this.setState({
-      contacts: this.state.contacts.filter(contact => contact.id !== id),
-    });
+    this.setState(prevState=>({
+      contacts: prevState.contacts.filter(contact => contact.id !== id),
+    }));
   };
 
   render() {
